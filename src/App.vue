3e670208247cd99c3f1c95933e87315e89c3ab41
taotaow2n <1669010607@qq.com>
2017-10-31 19:10:00 +0800

    'change'

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    	<!--指的是路由下的数据-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

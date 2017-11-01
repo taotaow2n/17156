80a56aeabbacc12bf1ecefe0ecdf15d7c7c6e718
taotaow2n <1669010607@qq.com>
2017-11-01 18:02:06 +0800

    'change'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  	<div class="header">
+  		这是header
+  	</div>
+</template>
+
+<script>
+	import HeaderComponent from "./Header"
+	export default {
+	components:{
+		"home-header":HeaderComponent
+	}
+}
+</script>
+
+<style>
+	.header{
+		height:70px;
+		background:#00bcd4;
+	}
+</style>

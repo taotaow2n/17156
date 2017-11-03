1830c65806e1d12909207a253f8210832fc24bde
taotaow2n <1669010607@qq.com>
2017-11-03 15:10:02 +0800

    'change'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  	<home-header></home-header>	
+	<div>
+ 		<home-header/>
+ 		<swiper-content/>
+ 		<menu-content/>
+ 	</div>
 </template>
 
 <script>
 
-import HeaderComponent from "./Header"
+import HeaderComponent from "./Header";
+import SwiperComponent from "./Swiper";
+import MenuComponent from "./Menu";
 
 export default {
 	components: {
-		"home-header": HeaderComponent
+		"home-header": HeaderComponent,
+		"swiper-content": SwiperComponent,
+		"menu-content":MenuComponent
 	}
 }
 </script>
 
 <style>
-	
 </style>

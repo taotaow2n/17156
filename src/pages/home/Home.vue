497bd253330f50397ed100dea1524e9f95b77011
lmj <11336964@qq.com>
2017-11-04 09:46:12 +0800

    'change'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,12 +2,12 @@
 	<div>
  		<home-header/>
  		<swiper-content/>
-<<<<<<< HEAD
+
  		<menu-content/>
  		<hot-recommend></hot-recommend>
-=======
+
  		<activity-content/>
->>>>>>> 7eabf3b377b645f6eeb4703baf6792b55efe748d
+
  	</div>
 </template>
 
@@ -15,23 +15,23 @@
 
 import HeaderComponent from "./Header";
 import SwiperComponent from "./Swiper";
-<<<<<<< HEAD
+
 import MenuComponent from "./Menu";
 import HotRecommendComponent from "./HotRecommend";
-=======
+
 import ActivityComponent from "./Activity";
->>>>>>> 7eabf3b377b645f6eeb4703baf6792b55efe748d
+
 
 export default {
 	components: {
 		"home-header": HeaderComponent,
 		"swiper-content": SwiperComponent,
-<<<<<<< HEAD
+
 		"menu-content":MenuComponent,
-		"hot-recommend":HotRecommendComponent 
-=======
+		"hot-recommend":HotRecommendComponent ,
+
 		"activity-content": ActivityComponent
->>>>>>> 7eabf3b377b645f6eeb4703baf6792b55efe748d
+
 	}
 }
 </script>
